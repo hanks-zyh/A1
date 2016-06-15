23bd8add4d2b62836c59c3ff24e86df7572836a6
hanks-zyh <zhangyuhan2014@gmail.com>
2016-06-16 00:09:44 +0800

    new titlebar style

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 854, height: 768, resizable: false })
+  mainWindow = new BrowserWindow({ width: 854, height: 768, resizable: false, title:'A1',titleBarStyle: 'hidden-inset' })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
